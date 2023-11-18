413fb0b6c7ff2bd3168de67b61e0b103d94d6c04
vasko34 <56612508+vasko34@users.noreply.github.com>
2023-11-18 15:48:34 +0200

    c

--- Challenge4-6/ViewController.swift
+++ Challenge4-6/ViewController.swift
@@ -1,17 +1,48 @@
-//
-//  ViewController.swift
-//  Challenge4-6
-//
-//  Created by User on 5.10.23.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var shoppingList = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        title = "Shopping List"
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshShoppingList))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return shoppingList.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = shoppingList[indexPath.row]
+        return cell
+    }
+    
+    @objc func refreshShoppingList() {
+        shoppingList = [String]()
+        tableView.reloadData()
+    }
+    
+    @objc func addTapped() {
+        let ac = UIAlertController(title: "Add Item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        ac.addAction(UIAlertAction(title: "Add", style: .default) { [weak self, weak ac] _ in
+            guard let item = ac?.textFields?[0].text else { return }
+            self?.submit(item)
+            
+        })
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func submit(_ item: String) {
+        shoppingList.insert(item, at: 0)
+        let indexPath = IndexPath(row: 0, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
     }
 
 
